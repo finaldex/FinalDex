2d7a25440a8fd54ad8965df81096223811769a30
finaldex <xedlanif@gmail.com>
2023-02-24 12:39:56 +0100

    Update mechanic.js

--- js/mechanic.js
+++ js/mechanic.js
@@ -1,45 +1,44 @@
 var createMechanic = function() {
 	var mechanicOuter = document.createElement("div");
-	var mechanicAside1 = document.createElement("aside");
-	var mechanicAside1OptionsTitleOuter = document.createElement("div");
-	var mechanicAside1OptionsTitle = document.createElement("h2");
-	var mechanicAside1OptionsSub = document.createElement("div");
-	var mechanicAside2 = document.createElement("aside");
-	var mechanicAside2Title = document.createElement("h1");
-	var mechanicAside3 = document.createElement("aside");
-	var mechanicAside3Content = document.createElement("div");
-	var mechanicAside4 = document.createElement("aside");
-	var mechanicAside4Icon = document.createElement("div");
-	var mechanicAside4IconImage = document.createElement("img");
-	mechanicOuter.setAttribute("id", "mechanic-outer");
-	mechanicOuter.setAttribute("name", "Mechanics");
-	mechanicAside1.setAttribute("id", "mechanic-aside1");
-	mechanicAside1OptionsTitleOuter.setAttribute("id", "mechanic-options-title");
-	mechanicAside1OptionsTitle.innerText = "Mechanics";
-	mechanicAside1OptionsSub.classList.add("mechanic-options-sub");
+	var mechanicSection1 = document.createElement("section");
+	var mechanicSection1OptionsTitleOuter = document.createElement("div");
+	var mechanicSection1OptionsTitle = document.createElement("h2");
+	var mechanicSection1OptionsSub = document.createElement("div");
+	var mechanicSection2 = document.createElement("section");
+	var mechanicSection2Title = document.createElement("h3");
+	var mechanicSection3 = document.createElement("section");
+	var mechanicSection3Content = document.createElement("div");
+	var mechanicSection4 = document.createElement("section");
+	var mechanicSection4Icon = document.createElement("div");
+	var mechanicSection4IconImage = document.createElement("img");
+	mechanicOuter.setAttribute("id", "mechanic");
+	mechanicOuter.setAttribute("value","mechanics");
+	mechanicSection1.setAttribute("id", "mechanic-section1");
+	mechanicSection1OptionsTitleOuter.setAttribute("id", "mechanic-options-title");
+	mechanicSection1OptionsTitle.innerText = "Mechanics";
 	var mechanicSub = ["Mechanics", "Features"];
 	for(var u = 0; u < mechanicSub.length; u++) {
 		var x = u + 1;
-		var mechanicAside1OptionsSubInput = document.createElement("input");
-		var mechanicAside1OptionsSubLabel = document.createElement("label");
-		mechanicAside1OptionsSubInput.setAttribute("type", "radio");
-		mechanicAside1OptionsSubInput.setAttribute("id", "mechanic-options-sub-selector-" + x);
-		mechanicAside1OptionsSubInput.setAttribute("name", "mechanic-options-sub-selector");
-		mechanicAside1OptionsSubInput.setAttribute("autocomplete", "off");
-		mechanicAside1OptionsSubInput.setAttribute("value", u);
-		mechanicAside1OptionsSubLabel.setAttribute("for", "mechanic-options-sub-selector-" + x);
-		mechanicAside1OptionsSubLabel.innerText = mechanicSub[u];
+		var mechanicSection1OptionsSubInput = document.createElement("input");
+		var mechanicSection1OptionsSubLabel = document.createElement("label");
+		mechanicSection1OptionsSubInput.setAttribute("type", "radio");
+		mechanicSection1OptionsSubInput.setAttribute("id", "mechanic-options-sub-selector-" + x);
+		mechanicSection1OptionsSubInput.setAttribute("name", "mechanic-options-sub-selector");
+		mechanicSection1OptionsSubInput.setAttribute("autocomplete", "off");
+		mechanicSection1OptionsSubInput.setAttribute("value", u);
+		mechanicSection1OptionsSubLabel.setAttribute("for", "mechanic-options-sub-selector-" + x);
+		mechanicSection1OptionsSubLabel.innerText = mechanicSub[u];
 		if(u == 0) {
-			mechanicAside1OptionsSubInput.setAttribute("checked", "");
+			mechanicSection1OptionsSubInput.setAttribute("checked", "");
 		}
-		mechanicAside1OptionsSub.appendChild(mechanicAside1OptionsSubInput);
-		mechanicAside1OptionsSub.appendChild(mechanicAside1OptionsSubLabel);
-		mechanicAside1OptionsSubInput.addEventListener("click", mechanicSubOptionsSelector);
+		mechanicSection1OptionsSub.appendChild(mechanicSection1OptionsSubInput);
+		mechanicSection1OptionsSub.appendChild(mechanicSection1OptionsSubLabel);
+		mechanicSection1OptionsSubInput.addEventListener("click", mechanicSubOptionsSelector);
 
 		function mechanicSubOptionsSelector() {
 			var mechanicOptionsOuters = document.querySelectorAll("#mechanic-options-outer[name]");
 			var mechanicOptionsOuter = document.querySelectorAll('#mechanic-options-outer[name="' + this.value + '"]');
-			mechanicAside1OptionsTitle.innerText = mechanicSub[this.value];
+			mechanicSection1OptionsTitle.innerText = mechanicSub[this.value];
 			for(var y = 0; y < mechanicOptionsOuters.length; y++) {
 				mechanicOptionsOuters[y].style.display = "none";
 			}
@@ -48,71 +47,70 @@ var createMechanic = function() {
 			}
 		}
 	}
-	mechanicAside2.setAttribute("id", "mechanic-aside2");
-	mechanicAside2Title.setAttribute("id", "mechanic-title");
-	mechanicAside3.setAttribute("id", "mechanic-aside3");
-	mechanicAside3Content.setAttribute("id", "mechanic-content");
-	mechanicAside3Content.classList.add("scroll");
-	mechanicAside4.setAttribute("id", "mechanic-aside4");
-	mechanicAside4Icon.classList.add("mechanic-descriptionicon");
-	mechanicAside4IconImage.src = "";
-	mechanicAside4IconImage.setAttribute("id", "mechanic-descriptioniconimg");
-	mechanicAside4IconImage.setAttribute("onerror","if(this.getAttribute('srced') == null){this.src='./media/Images/Misc/FinalDex/Catching.png';this.setAttribute('srced','');}");
-	mechanicAside4IconImage.setAttribute("onload","if(this.getAttribute('srced') != null){this.removeAttribute('srced')};");
+	mechanicSection2.setAttribute("id", "mechanic-section2");
+	mechanicSection2Title.setAttribute("id", "mechanic-title");
+	mechanicSection3.setAttribute("id", "mechanic-section3");
+	mechanicSection3Content.setAttribute("id", "mechanic-content");
+	mechanicSection3Content.classList.add("scroll");
+	mechanicSection4.setAttribute("id", "mechanic-section4");
+	mechanicSection4IconImage.src = "";
+	mechanicSection4IconImage.setAttribute("id", "mechanic-descriptioniconimg");
+	mechanicSection4IconImage.setAttribute("onerror","if(this.getAttribute('srced') == null){this.src='./media/Images/Misc/FinalDex/Catching.png';this.setAttribute('srced','');}");
+	mechanicSection4IconImage.setAttribute("onload","if(this.getAttribute('srced') != null){this.removeAttribute('srced')};");
 	document.querySelector("#contain").appendChild(mechanicOuter);
-	mechanicOuter.appendChild(mechanicAside1);
-	mechanicAside1.appendChild(mechanicAside1OptionsTitleOuter);
-	mechanicAside1OptionsTitleOuter.appendChild(mechanicAside1OptionsTitle);
-	mechanicAside1.appendChild(mechanicAside1OptionsSub);
-	mechanicOuter.appendChild(mechanicAside2);
-	mechanicAside2.appendChild(mechanicAside2Title);
-	mechanicOuter.appendChild(mechanicAside3);
-	mechanicAside3.appendChild(mechanicAside3Content);
-	mechanicOuter.appendChild(mechanicAside4);
-	mechanicAside4.appendChild(mechanicAside4Icon);
-	mechanicAside4Icon.appendChild(mechanicAside4IconImage);
+	mechanicOuter.appendChild(mechanicSection1);
+	mechanicSection1.appendChild(mechanicSection1OptionsTitleOuter);
+	mechanicSection1OptionsTitleOuter.appendChild(mechanicSection1OptionsTitle);
+	mechanicSection1.appendChild(mechanicSection1OptionsSub);
+	mechanicOuter.appendChild(mechanicSection2);
+	mechanicSection2.appendChild(mechanicSection2Title);
+	mechanicOuter.appendChild(mechanicSection3);
+	mechanicSection3.appendChild(mechanicSection3Content);
+	mechanicOuter.appendChild(mechanicSection4);
+	mechanicSection4.appendChild(mechanicSection4Icon);
+	mechanicSection4Icon.appendChild(mechanicSection4IconImage);
 
-    var mechanicAside2Game = document.createElement("span");
-    var mechanicAside2GameImage = document.createElement("img");
-    mechanicAside2GameImage.src = "./media/Images/Misc/Title/Text/" + GameFullName.replaceAll(",", "").replaceAll("!", "").replaceAll("'", "").replaceAll(":", "") + ".png";
-    mechanicAside2GameImage.setAttribute("onerror","this.display='none'");
-    mechanicAside2.appendChild(mechanicAside2Game);
-    mechanicAside2Game.appendChild(mechanicAside2GameImage);
+    var mechanicSection2Game = document.createElement("span");
+    var mechanicSection2GameImage = document.createElement("img");
+    mechanicSection2GameImage.src = "./media/Images/Misc/Title/Text/" + GameFullName.replaceAll(",", "").replaceAll("!", "").replaceAll("'", "").replaceAll(":", "") + ".png";
+    mechanicSection2GameImage.setAttribute("onerror","this.display='none'");
+    mechanicSection2.appendChild(mechanicSection2Game);
+    mechanicSection2Game.appendChild(mechanicSection2GameImage);
 
 
 	for(var i = 0; i < mechanicOptionsTitle.length; i++) {
-		var mechanicAside1OptionsOuter = document.createElement("div");
-		var mechanicAside1Options = document.createElement("div");
-		mechanicAside1OptionsOuter.setAttribute("id", "mechanic-options-outer");
-		mechanicAside1OptionsOuter.classList.add("scroll");
-		mechanicAside1OptionsOuter.setAttribute("name", i);
-		mechanicAside1Options.setAttribute("id", "mechanic-options");
-		mechanicAside1.appendChild(mechanicAside1OptionsOuter);
-		mechanicAside1OptionsOuter.appendChild(mechanicAside1Options);
+		var mechanicSection1OptionsOuter = document.createElement("div");
+		var mechanicSection1Options = document.createElement("div");
+		mechanicSection1OptionsOuter.setAttribute("id", "mechanic-options-outer");
+		mechanicSection1OptionsOuter.classList.add("scroll");
+		mechanicSection1OptionsOuter.setAttribute("name", i);
+		mechanicSection1Options.setAttribute("id", "mechanic-options");
+		mechanicSection1.appendChild(mechanicSection1OptionsOuter);
+		mechanicSection1OptionsOuter.appendChild(mechanicSection1Options);
 		for(var q = 0; q < mechanicOptionsTitle[i].length; q++) {
-			var mechanicAside1OptionsInput = document.createElement("input");
-			var mechanicAside1OptionsLabel = document.createElement("label");
-			mechanicAside1OptionsInput.setAttribute("type", "radio");
-			mechanicAside1OptionsInput.setAttribute("name", "mechanic-options");
-			mechanicAside1OptionsInput.setAttribute("id", "mechanic-options-" + i + "-" + q);
-			mechanicAside1OptionsInput.setAttribute("autocomplete", "off");
-			mechanicAside1OptionsInput.value = q;
-			mechanicAside1OptionsInput.alt = i;
-			mechanicAside1OptionsLabel.setAttribute("for", "mechanic-options-" + i + "-" + q);
-			mechanicAside1OptionsLabel.setAttribute("name", "large");
-			mechanicAside1OptionsLabel.innerText = mechanicOptionsTitle[i][q];
-			mechanicAside1Options.appendChild(mechanicAside1OptionsInput);
-			mechanicAside1Options.appendChild(mechanicAside1OptionsLabel);
-			mechanicAside1OptionsInput.addEventListener("click", mechanicOptionsSelector);
+			var mechanicSection1OptionsInput = document.createElement("input");
+			var mechanicSection1OptionsLabel = document.createElement("label");
+			mechanicSection1OptionsInput.setAttribute("type", "radio");
+			mechanicSection1OptionsInput.setAttribute("name", "mechanic-options");
+			mechanicSection1OptionsInput.setAttribute("id", "mechanic-options-" + i + "-" + q);
+			mechanicSection1OptionsInput.setAttribute("autocomplete", "off");
+			mechanicSection1OptionsInput.value = q;
+			mechanicSection1OptionsInput.alt = i;
+			mechanicSection1OptionsLabel.setAttribute("for", "mechanic-options-" + i + "-" + q);
+			mechanicSection1OptionsLabel.setAttribute("type","large");
+			mechanicSection1OptionsLabel.innerText = mechanicOptionsTitle[i][q];
+			mechanicSection1Options.appendChild(mechanicSection1OptionsInput);
+			mechanicSection1Options.appendChild(mechanicSection1OptionsLabel);
+			mechanicSection1OptionsInput.addEventListener("click", mechanicOptionsSelector);
 
 			function mechanicOptionsSelector() {
-				mechanicAside2Title.innerText = mechanicOptionsTitle[this.alt][this.value];
-				mechanicAside3Content.innerHTML = mechanicOptions[this.alt][this.value];
-				mechanicAside4IconImage.src = "./media/Images/Misc/FinalDex/" + mechanicOptionsTitle[this.alt][this.value] + ".png";
-				mechanicAside4IconImage.style.display = "unset";
+				mechanicSection2Title.innerText = mechanicOptionsTitle[this.alt][this.value];
+				mechanicSection3Content.innerHTML = mechanicOptions[this.alt][this.value];
+				mechanicSection4IconImage.src = "./media/Images/Misc/FinalDex/" + mechanicOptionsTitle[this.alt][this.value] + ".png";
+				mechanicSection4IconImage.style.display = "unset";
 			}
 			if(i == 0 && q == 0) {
-				mechanicAside1OptionsLabel.click();
+				mechanicSection1OptionsLabel.click();
 			}
 		}
 	}
