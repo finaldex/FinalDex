aa61daefbb9ffceb4b51476f7c1fd50d0c641b84
finaldex <xedlanif@gmail.com>
2023-06-06 18:40:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
     function openModal() {
 
-        var modalTrigger = document.getElementsByClassName('bug-modal-open');
+        let modalTrigger = document.getElementsByClassName('bug-modal-open');
 
 
-        for (var i = 0; i < modalTrigger.length; i++) {
+        for (let i = 0; i < modalTrigger.length; i++) {
             modalTrigger[i].onclick = function() {
-                var modalWindow = document.getElementById("bug-modal");
+                let modalWindow = document.getElementById("bug-modal");
 
                 $("body").addClass("modal-open");
 
@@ -18,12 +18,12 @@
 
     function closeModal() {
 
-        var closeButton = document.getElementsByClassName('bug-modal-close');
-        var closeOverlay = document.getElementsByClassName('bug-modal-overlay');
+        let closeButton = document.getElementsByClassName('bug-modal-close');
+        let closeOverlay = document.getElementsByClassName('bug-modal-overlay');
 
-        for (var i = 0; i < closeButton.length; i++) {
+        for (let i = 0; i < closeButton.length; i++) {
             closeButton[i].onclick = function() {
-                var modalWindow = this.parentNode.parentNode;
+                let modalWindow = this.parentNode.parentNode;
 
                 $("body").removeClass("modal-open");
 
@@ -31,9 +31,9 @@
             }
         }
 
-        for (var i = 0; i < closeOverlay.length; i++) {
+        for (let i = 0; i < closeOverlay.length; i++) {
             closeOverlay[i].onclick = function() {
-                var modalWindow = this.parentNode;
+                let modalWindow = this.parentNode;
 
                 $("body").removeClass("modal-open");
 
@@ -61,10 +61,10 @@
 (function() {
     // get all data in form and return object
     function getFormData(form) {
-      var elements = form.elements;
-      var honeypot;
+      let elements = form.elements;
+      let honeypot;
   
-      var fields = Object.keys(elements).filter(function(k) {
+      let fields = Object.keys(elements).filter(function(k) {
         if (elements[k].name === "honeypot") {
           honeypot = elements[k].value;
           return false;
@@ -81,18 +81,18 @@
         return self.indexOf(item) == pos && item;
       });
   
-      var formData = {};
+      let formData = {};
       fields.forEach(function(name){
-        var element = elements[name];
+        let element = elements[name];
         
         // singular form elements just have one value
         formData[name] = element.value;
   
         // when our element has multiple items, get their values
         if (element.length) {
-          var data = [];
-          for (var i = 0; i < element.length; i++) {
-            var item = element.item(i);
+          let data = [];
+          for (let i = 0; i < element.length; i++) {
+            let item = element.item(i);
             if (item.checked || item.selected) {
               data.push(item.value);
             }
@@ -112,9 +112,9 @@
   
     function handleFormSubmit(event) {  // handles form submit without any jquery
       event.preventDefault();           // we are submitting via xhr below
-      var form = event.target;
-      var formData = getFormData(form);
-      var data = formData.data;
+      let form = event.target;
+      let formData = getFormData(form);
+      let data = formData.data;
   
       // If a honeypot field is filled, assume it was done so by a spam bot.
       if (formData.honeypot) {
@@ -122,26 +122,26 @@
       }
   
       disableAllButtons(form);
-      var url = form.action;
-      var xhr = new XMLHttpRequest();
+      let url = form.action;
+      let xhr = new XMLHttpRequest();
       xhr.open('POST', url);
       // xhr.withCredentials = true;
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xhr.onreadystatechange = function() {
           if (xhr.readyState === 4 && xhr.status === 200) {
             form.reset();
-            var formElements = form.querySelector(".form-elements")
+            let formElements = form.querySelector(".form-elements")
             if (formElements) {
               formElements.style.display = "none"; // hide form
             }
-            var thankYouMessage = form.querySelector(".thankyou_message");
+            let thankYouMessage = form.querySelector(".thankyou_message");
             if (thankYouMessage) {
               thankYouMessage.style.display = "flex";
             }
           }
       };
       // url encode form data for sending as post data
-      var encoded = Object.keys(data).map(function(k) {
+      let encoded = Object.keys(data).map(function(k) {
           return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
       }).join('&');
       xhr.send(encoded);
@@ -149,16 +149,16 @@
     
     function loaded() {
       // bind to the submit event of our form
-      var forms = document.querySelectorAll("form.gform");
-      for (var i = 0; i < forms.length; i++) {
+      let forms = document.querySelectorAll("form.gform");
+      for (let i = 0; i < forms.length; i++) {
         forms[i].addEventListener("submit", handleFormSubmit, false);
       }
     };
     document.addEventListener("DOMContentLoaded", loaded, false);
   
     function disableAllButtons(form) {
-      var buttons = form.querySelectorAll("button");
-      for (var i = 0; i < buttons.length; i++) {
+      let buttons = form.querySelectorAll("button");
+      for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
       }
     }
