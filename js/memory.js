6a36b92229b032ca594060f01f2fcfb470acba9c
finaldex <xedlanif@gmail.com>
2023-06-06 18:40:20 +0200

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,8 @@
 function partyMemory(action) {
-    var action;
-    var partySlots = document.querySelectorAll('#pokémon > aside[name="team"] section[name="party"] > div');
+    let partySlots = document.querySelectorAll('#pokémon > aside[name="team"] section[name="party"] > div');
     if (action == "Save") {
-        var partyMemory = [];
-        for(var i = 0; i < partySlots.length; i++) {
+        let partyMemory = [];
+        for(let i = 0; i < partySlots.length; i++) {
             if (partySlots[i].getAttribute("name") != "empty") {
                 partyMemory.push(getPartyData(partySlots[i]))
             }
@@ -14,11 +13,11 @@ function partyMemory(action) {
         localStorage.setItem("finaldex-party-"+GameID, partyMemory.join("/"));
     }
     else if (action == "Restore") {
-        var tempArr = localStorage.getItem("finaldex-party-"+GameID);
+        let tempArr = localStorage.getItem("finaldex-party-"+GameID);
         if (tempArr != undefined) {
             tempArr = localStorage.getItem("finaldex-party-"+GameID).split("/");
             if (tempArr.length == partySlots.length) {
-                for(var i = 0; i < tempArr.length; i++) {
+                for(let i = 0; i < tempArr.length; i++) {
                     if (tempArr[i] != "") {
                         createParty(partySlots[i],tempArr[i]);
                         partyShow(partySlots[i]);
@@ -33,25 +32,24 @@ function partyMemory(action) {
 
 
 function boxMemory(action) {
-    var action;
 
     if (action == "Save") {
         localStorage.setItem("finaldex-box-"+GameID, getAllBoxData());
     }
     else if (action == "Restore") {
-        var tempArr = [];
-        var tempStr = localStorage.getItem("finaldex-box-"+GameID);
+        let tempArr = [];
+        let tempStr = localStorage.getItem("finaldex-box-"+GameID);
 
         if (tempStr != "undefined" && tempStr != undefined) {
             if (tempStr.includes("/")) {
                 tempArr = tempStr.split("/");
-                for(var i = 0; i < tempArr.length; i++) {
+                for(let i = 0; i < tempArr.length; i++) {
                     storeInBox(tempArr[i]);
                 }
             }
             else {
                 tempArr[0] = tempStr;
-                for(var i = 0; i < tempArr.length; i++) {
+                for(let i = 0; i < tempArr.length; i++) {
                     storeInBox(tempArr[i]);
                 }
             }
@@ -62,12 +60,8 @@ function boxMemory(action) {
 
 
 function memory(action,suffix,elements) {
-	
-	var action;
-	var elements;
-    var suffix
 
-	var tempArr = [];
+	let tempArr = [];
 
 	tempArr.push("finaldex");
     if (suffix.includes("game")) {
@@ -75,9 +69,9 @@ function memory(action,suffix,elements) {
     }
 	
 
-	for(var i = 0; i < elements.length; i++) {
-		var setName = tempArr.join("-")+"-"+elements[i].getAttribute("name");
-		var setValue;
+	for(let i = 0; i < elements.length; i++) {
+		let setName = tempArr.join("-")+"-"+elements[i].getAttribute("name");
+		let setValue;
 
 		if (elements[i].tagName == "INPUT" && elements[i].getAttribute("type") == "checkbox") {
 			setValue = elements[i].checked;
@@ -91,13 +85,13 @@ function memory(action,suffix,elements) {
 		}
 		else if (action == "Restore") {
             if (elements[i].tagName == "INPUT" && elements[i].getAttribute("type") == "checkbox") {
-                var item = JSON.parse(localStorage.getItem(setName));
+                let item = JSON.parse(localStorage.getItem(setName));
                 if (item != null) {
                     elements[i].checked = JSON.parse(localStorage.getItem(setName));
                 }
             }
             else {
-                var item = localStorage.getItem(setName);
+                let item = localStorage.getItem(setName);
                 if (item != null) {
                     elements[i].value = item;
                 }
@@ -110,11 +104,11 @@ function memory(action,suffix,elements) {
 
 
 function memoryDexSwitch() {
-	var radio = document.getElementsByName("finaldex-dexswitch-" + GameID);
-	var val = localStorage.getItem("finaldex-dexswitch-" + GameID);
+	let radio = document.getElementsByName("finaldex-dexswitch-" + GameID);
+	let val = localStorage.getItem("finaldex-dexswitch-" + GameID);
 
 
-	for(var i = 0; i < radio.length; i++) {
+	for(let i = 0; i < radio.length; i++) {
 		if(radio[i].value == val) {
 			radio[i].click();
 		} else if(val == null) {
