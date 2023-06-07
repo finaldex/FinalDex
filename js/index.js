ea1dc3f9f974804d38ef2dcd334ead36da821eb0
finaldex <xedlanif@gmail.com>
2023-06-07 19:59:21 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,56 +2,68 @@ let wsrc = ["Home","Games","FAQ","Bug","Metadata","Sources","Changelog"]
 
 function build() {
 
-	let GamesFull = ["random","Red Version","Blue Version","Yellow Version","Gold Version","Silver Version","Crystal Version","Ruby Version","Sapphire Version","Colosseum","FireRed Version","LeafGreen Version","Emerald Version","XD: Gale of Darkness","Diamond Version","Pearl Version","Platinum Version","HeartGold Version","SoulSilver Version","Black Version","White Version","Black Version 2","White Version 2","X","Y","Omega Ruby","Alpha Sapphire","Sun","Moon","Ultra Sun","Ultra Moon","Let's Go, Pikachu!","Let's Go, Eevee!","Sword","Shield"];
-	let GamesSimple = ["random","Red Version","Blue Version","Yellow Version","Gold Version","Silver Version","Crystal Version","Ruby Version","Sapphire Version","Colosseum","FireRed Version","LeafGreen Version","Emerald Version","XD Gale of Darkness","Diamond Version","Pearl Version","Platinum Version","HeartGold Version","SoulSilver Version","Black Version","White Version","Black Version 2","White Version 2","X","Y","Omega Ruby","Alpha Sapphire","Sun","Moon","Ultra Sun","Ultra Moon","Lets Go Pikachu","Lets Go Eevee","Sword","Shield"];
-	let GamesGeneration = [0,1,1,1,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,7,7,8,8]
-	let GamesArt = ["","Kanto_RBY","Kanto_RBY","Kanto_RBY","JohtoKanto_GSC","JohtoKanto_GSC","JohtoKanto_GSC","Hoenn_RS","Hoenn_RS","","Kanto_FRLG","Kanto_FRLG","Hoenn_E","","Sinnoh_DP","Sinnoh_DP","Sinnoh_Pt","JohtoKanto_HGSS","JohtoKanto_HGSS","Unova_BW","Unova_BW","Unova_B2W2","Unova_B2W2","Kalos_XY","Kalos_XY","Hoenn_ORAS","Hoenn_ORAS","Alola_SM","Alola_SM","Alola_USUM","Alola_USUM","Kanto_LGPE","Kanto_LGPE","Galar_SwSh","Galar_SwSh"];
-	let GamesTitle = ["Random","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+	let Games = []
 
-
-
-
-
-	for (let i = 0; i < GamesFull.length; i++) {
-			let x = i;
-			let li = document.createElement("li");
-			li.name = x;
-			li.addEventListener("keyup",function(event){if(event.which === 13){event.target.querySelector(":scope a").click();}});
-			document.querySelector("#Games span ul[name='"+GamesGeneration[i]+"']").appendChild(li);
-			li.setAttribute("tabindex","1");
-
-
-			if (GamesArt[i] != "") {
-				let art = document.createElement("img");
-				art.src = "./media/Images/Misc/Region/" + GamesArt[i] + ".png";
-				art.title = GamesArt[i];
-				art.setAttribute("onerror","this.style.display='none'");
-				li.appendChild(art)
-			}
-			else {
-					li.classList.add("miss");
-			}
-
-
-
-			let titlewrap = document.createElement("a");
-			let title = document.createElement("img");
-			title.src = "./media/Images/Misc/Title/Text/" + GamesSimple[i] + ".png";
-			title.title = GamesSimple[i];
-			title.setAttribute("onerror","this.style.display='none'");
-			titlewrap.setAttribute("href","./Game.html"+"#"+GamesFull[i]);
-			titlewrap.setAttribute("tabindex","-1");
-			li.appendChild(titlewrap)
-			titlewrap.appendChild(title)
-
-			if (GamesTitle[i] != "") {
-				let h = document.createElement("h1");
-				h.innerText = GamesTitle[i];
-				titlewrap.appendChild(h);
-			}
-
+	for (let i = 0; i < finaldata["Game"]["Reference"].length; i++) {
+		let type = finaldata["Game"]["Reference"][i]["Type"]
+		if (type == "Core" || type == "Side") {
+			let obj = new Object();
+			obj["Name"] = finaldata["Game"]["Reference"][i]["Name"];
+			obj["ID"] = finaldata["Game"]["Reference"][i]["ID"];
+			obj["Generation"] = finaldata["Game"]["Reference"][i]["Generation"];
+			Games.push(obj)
+		}
 	}
 
+	let ranobj = new Object();
+	ranobj["Name"] = "Random";
+	ranobj["ID"] = 0;
+	ranobj["Generation"] = 0;
+	Games.push(ranobj)
+
+
+	console.log(finaldata)
+	console.log(Games)
+	for (let i = 0; i < Games.length; i++) {
+		let generation = Games[i]["Generation"];
+		let id = Games[i]["ID"];
+		let name = Games[i]["Name"];
+
+		let appender = document.querySelector("#Games span ul[name='"+generation+"']");
+
+
+		let li = document.createElement("li");
+		li.setAttribute("name",id);
+		li.addEventListener("keyup",function(event){if(event.which === 13){event.target.querySelector(":scope a").click();}});
+		appender.appendChild(li);
+		li.setAttribute("tabindex","1");
+
+
+
+
+		let art = document.createElement("img");
+		art.src = getMedia([""],[PATH_Region_Art_Official])[0];
+		li.appendChild(art)
+
+
+		let titlewrap = document.createElement("a");
+		let title = document.createElement("img");
+		title.src = getMedia([name],[PATH_Game_Title])[0];
+		title.title = name;
+		titlewrap.setAttribute("href","./Game.html#"+name);
+		titlewrap.setAttribute("tabindex","-1");
+		li.appendChild(titlewrap)
+		titlewrap.appendChild(title)
+
+		if (id == 0) {
+			let h = document.createElement("h1");
+			h.innerText = name;
+			titlewrap.appendChild(h);
+		}
+	}
+
+
+	/*
 	let arr1 = [];
   	let arr2 = [];
 
@@ -117,146 +129,169 @@ function build() {
       div.appendChild(img);
     }
   }
-
+*/
 
 
 }
 
 
-build();
+let finaldata = [];
+let baseurl = "https://raw.githubusercontent.com/finaldex/FinalDex/main/data/";
+let baseextension = "json";
+
+let datas = ["Directory","Game Metadata"]
+for(let i = 0; i < datas.length; i++) {
+    loadData(i)
+}
+
+
+function loadData(i) {
+    let val = datas[i].replace(" Metadata","")
+    let gameRequest = new XMLHttpRequest();
+    gameRequest.open('GET', baseurl+datas[i]+"."+baseextension);
+    gameRequest.responseType = 'json';
+    gameRequest.send();
+    gameRequest.onload = function() {
+        finaldata[val] = gameRequest.response;
+		if (i == datas.length-1) {
+			build();
+		}
+    }
+}
+
+
+
 
 $(window).on('beforeunload', function() {
-		$(window).scrollTop(0);
+	$(window).scrollTop(0);
 });
 
 (function() {
-		// get all data in form and return object
-		function getFormData(form) {
-			let elements = form.elements;
-			let honeypot;
-	
-			let fields = Object.keys(elements).filter(function(k) {
-				if (elements[k].name === "hp") {
-					honeypot = elements[k].value;
-					return false;
-				}
-				return true;
-			}).map(function(k) {
-				if(elements[k].name !== undefined) {
-					return elements[k].name;
-				// special case for Edge's html collection
-				}else if(elements[k].length > 0){
-					return elements[k].item(0).name;
-				}
-			}).filter(function(item, pos, self) {
-				return self.indexOf(item) == pos && item;
-			});
-	
-			let formData = {};
-			fields.forEach(function(name){
-				let element = elements[name];
-				
-				// singular form elements just have one value
-				formData[name] = element.value;
-	
-				// when our element has multiple items, get their values
-				if (element.length) {
-					let data = [];
-					for (let i = 0; i < element.length; i++) {
-						let item = element.item(i);
-						if (item.checked || item.selected) {
-							data.push(item.value);
-						}
+	// get all data in form and return object
+	function getFormData(form) {
+		let elements = form.elements;
+		let honeypot;
+
+		let fields = Object.keys(elements).filter(function(k) {
+			if (elements[k].name === "hp") {
+				honeypot = elements[k].value;
+				return false;
+			}
+			return true;
+		}).map(function(k) {
+			if(elements[k].name !== undefined) {
+				return elements[k].name;
+			// special case for Edge's html collection
+			}else if(elements[k].length > 0){
+				return elements[k].item(0).name;
+			}
+		}).filter(function(item, pos, self) {
+			return self.indexOf(item) == pos && item;
+		});
+
+		let formData = {};
+		fields.forEach(function(name){
+			let element = elements[name];
+			
+			// singular form elements just have one value
+			formData[name] = element.value;
+
+			// when our element has multiple items, get their values
+			if (element.length) {
+				let data = [];
+				for (let i = 0; i < element.length; i++) {
+					let item = element.item(i);
+					if (item.checked || item.selected) {
+						data.push(item.value);
 					}
-					formData[name] = data.join(', ');
 				}
-			});
-			// add form-specific values into the data
-			formData.formDataNameOrder = JSON.stringify(fields);
-			formData.formGoogleSheetName = form.dataset.sheet || "reports"; // default sheet name
-			formData.formGoogleSendEmail
-				= form.dataset.email || ""; // no email by default
-	
-			return {data: formData, honeypot: honeypot};
-		}
-	
-		function handleFormSubmit(event) {	// handles form submit without any jquery
-			event.preventDefault();					 // we are submitting via xhr below
-			let form = event.target;
-			let formData = getFormData(form);
-			let data = formData.data;
-			let textarea = form.querySelector(":scope textarea");
+				formData[name] = data.join(', ');
+			}
+		});
+		// add form-specific values into the data
+		formData.formDataNameOrder = JSON.stringify(fields);
+		formData.formGoogleSheetName = form.dataset.sheet || "reports"; // default sheet name
+		formData.formGoogleSendEmail
+			= form.dataset.email || ""; // no email by default
+
+		return {data: formData, honeypot: honeypot};
+	}
+
+	function handleFormSubmit(event) {	// handles form submit without any jquery
+		event.preventDefault();					 // we are submitting via xhr below
+		let form = event.target;
+		let formData = getFormData(form);
+		let data = formData.data;
+		let textarea = form.querySelector(":scope textarea");
 
 
-	
-			// If a honeypot field is filled, assume it was done so by a spam bot.
-			if (formData.honeypot) {
-				return false;
-			}
-			else if (textarea.value == "" || textarea.value == undefined ) {
-				form.querySelector(":scope button").animate([{transform: "translateX(0px)"}, {transform: "translateX(2px)"}, {transform: "translateX(0px)"}, {transform: "translateX(-2px)"}, {transform: "translateX(0px)"}, ], {duration: 200,easing: "linear",iterations: 1,});
-				return false;
-			}
-			else {
-				buttonAction(form,true);
-				let url = form.action;
-				let xhr = new XMLHttpRequest();
-				xhr.open('POST', url);
-				// xhr.withCredentials = true;
-				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-				xhr.onreadystatechange = function() {
-						if (xhr.readyState === 4 && xhr.status === 200) {
-								form.reset();
-								let formElements = form.querySelector(".gform > span")
-								if (formElements) {
-								formElements.classList.add("hide");
-								}
-						}
-				};
-				// url encode form data for sending as post data
-				let encoded = Object.keys(data).map(function(k) {
-						return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-				}).join('&');
-				xhr.send(encoded);
-			}
+
+		// If a honeypot field is filled, assume it was done so by a spam bot.
+		if (formData.honeypot) {
+			return false;
 		}
-		
-		function loaded() {
-			// bind to the submit event of our form
-			let forms = document.querySelectorAll("form.gform");
-			for (let i = 0; i < forms.length; i++) {
-				forms[i].addEventListener("submit", handleFormSubmit, false);
-			}
-		};
-		document.addEventListener("DOMContentLoaded", loaded, false);	
+		else if (textarea.value == "" || textarea.value == undefined ) {
+			form.querySelector(":scope button").animate([{transform: "translateX(0px)"}, {transform: "translateX(2px)"}, {transform: "translateX(0px)"}, {transform: "translateX(-2px)"}, {transform: "translateX(0px)"}, ], {duration: 200,easing: "linear",iterations: 1,});
+			return false;
+		}
+		else {
+			buttonAction(form,true);
+			let url = form.action;
+			let xhr = new XMLHttpRequest();
+			xhr.open('POST', url);
+			// xhr.withCredentials = true;
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xhr.onreadystatechange = function() {
+					if (xhr.readyState === 4 && xhr.status === 200) {
+							form.reset();
+							let formElements = form.querySelector(".gform > span")
+							if (formElements) {
+							formElements.classList.add("hide");
+							}
+					}
+			};
+			// url encode form data for sending as post data
+			let encoded = Object.keys(data).map(function(k) {
+					return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+			}).join('&');
+			xhr.send(encoded);
+		}
+	}
+	
+	function loaded() {
+		// bind to the submit event of our form
+		let forms = document.querySelectorAll("form.gform");
+		for (let i = 0; i < forms.length; i++) {
+			forms[i].addEventListener("submit", handleFormSubmit, false);
+		}
+	};
+	document.addEventListener("DOMContentLoaded", loaded, false);	
 })();
 
 function buttonAction(form,action) {
-		let action;
-		let buttons = form.querySelectorAll(":scope div:first-child button");
-		for (let i = 0; i < buttons.length; i++) {
+	let buttons = form.querySelectorAll(":scope div:first-child button");
+	for (let i = 0; i < buttons.length; i++) {
 		buttons[i].disabled = action;
-		}
+	}
 }
 
 $(document).ready(function() {
+	let stickyNavTop = $('#content').offset().top;
 
-		let stickyNavTop = $('#content').offset().top;
+	let stickyNav = function(){
+			let scrollTop = $('#content').scrollTop();
+							
 
-		let stickyNav = function(){
-				let scrollTop = $('#content').scrollTop();
-								
+			if (scrollTop > stickyNavTop) {
+					$('nav').addClass('sticky');
+			} else {
+					$('nav').removeClass('sticky');
+			}
+	};
 
-				if (scrollTop > stickyNavTop) {
-						$('nav').addClass('sticky');
-				} else {
-						$('nav').removeClass('sticky');
-				}
-		};
+	stickyNav();
 
-		stickyNav();
-
-		$('#content').scroll(function() {
-				stickyNav();
-		});
+	$('#content').scroll(function() {
+			stickyNav();
+	});
 });
