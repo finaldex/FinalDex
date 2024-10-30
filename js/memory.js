1c680218c4c69830ee04f3c02e119ec7850696b8
finaldex <123470609+finaldex@users.noreply.github.com>
2024-10-30 15:54:34 +0100

    Update memory.js

--- js/memory.js
+++ js/memory.js
@@ -1,129 +1,71 @@
-function partyMemory(action) {
-    let partySlots = document.querySelectorAll('#pokémon > aside[name="team"] section[name="party"] > div');
-    if (action == "Save") {
-        let partyMemory = [];
-        for(let i = 0; i < partySlots.length; i++) {
-            partyMemory.push(partySlots[i].getAttribute("data-string"))
+// Function to save input status to memory and localStorage
+function memory_save({ Elements, Group }) {
+    // Navigate to the correct nested memory object
+    let target = memory;
+    Group.forEach(g => {
+        if (!target[g]) {
+            target[g] = {};
         }
-        localStorage.setItem("finaldex-party-"+GameID, partyMemory.join("/"));
-    }
-    else if (action == "Restore") {
-        let tempData = localStorage.getItem("finaldex-party-"+GameID);
-        if (tempData != undefined) {
-            let tempArr = localStorage.getItem("finaldex-party-"+GameID).split("/");
-            if (tempArr.length == partySlots.length) {
-                for(let i = 0; i < tempArr.length; i++) {
-                    if (tempArr[i] != "") {
-                        partySlots[i].setAttribute("data-string",tempArr[i])
-                        partyApply(partySlots[i]);
-                    }
-                }
-            }
+        target = target[g];
+    });
+
+    Elements.forEach(element => {
+        const id = element.id;
+        const type = element.type;
+        const value = element.value;
+
+        if (type === "checkbox") {
+            target[id] = element.checked; // Save checked status
+        } else if (type === "radio" && element.checked) {
+            target[id] = value; // Save the checked radio value
+        } else if (type === "number" || type === "text") {
+            target[id] = value; // Save the value for number and text inputs
         }
-    }
+    });
+
+    // Update localStorage
+    localStorage.setItem('finaldex-memory', JSON.stringify(memory));
 }
 
+// Function to restore input states from memory
+function memory_restore({ Elements, Group }) {
+    // Navigate to the correct nested memory object
+    let target = memory;
+    Group.forEach(g => {
+        if (!target[g]) {
+            target[g] = {};
+        }
+        target = target[g];
+    });
 
+    Elements.forEach(element => {
+        const id = element.id;
+        const type = element.type;
 
-
-function boxMemory(action) {
-
-    if (action == "Save") {
-        let els = document.querySelectorAll("#pokémon > aside section[name='box'] ul > li");
-        let data = [];
-        for (let i = 0; i < els.length; i++) {
-            let data_str = els[i].getAttribute("data-string")
-            if (data_str != undefined && data_str != "") {
-                data.push(data_str);
+        if (target[id] !== undefined) {
+            if (type === "checkbox") {
+                element.checked = target[id]; // Set checked status based on memory
+            } else if (type === "radio") {
+                element.checked = (target[id] === element.value); // Set checked for the saved radio value
+            } else if (type === "number" || type === "text") {
+                element.value = target[id]; // Restore value for number and text inputs
             }
         }
-        localStorage.setItem("finaldex-box-"+GameID, JSON.stringify(data));
-    }
-    else if (action == "Restore") {
-        let data = localStorage.getItem("finaldex-box-"+GameID);
-        if (data != undefined && data != "") {
-            try {
-                data = JSON.parse(data)
-                for (let i = 0; i < data.length; i++) {
-                    createBox(data[i]);
-                }
-            }
-            catch {
-                console.log(data)
-            }
-        }
-    }
-
+    });
 }
 
+const create_memory = function() {
+    // Initialize memory from localStorage or set default
+    memory = JSON.parse(localStorage.getItem('finaldex-memory')) || {};
 
-function memory(action,suffix,elements) {
+    if (!memory[config.Game]) memory[config.Game] = {};
+    if (!memory[config.Game].Dex) memory[config.Game].Dex = {};
 
-	let tempArr = [];
+    const pokemon = document.querySelectorAll('#dex .dex_list li input'); // Select all relevant inputs
 
-	tempArr.push("finaldex");
-    if (suffix.includes("game")) {
-        tempArr.push(GameID);
-    }
+    pokemon.forEach(input => {
+        input.addEventListener('change', () => memory_save({ Elements: pokemon, Group: [config.Game, 'Dex'] })); // Save status on change
+    });
 
-  
-
-	for(let i = 0; i < elements.length; i++) {
-		let setName = tempArr.join("-")+"-"+elements[i].getAttribute("name");
-		let setValue;
-
-        if (setName.includes("null")) {
-            console.warn("Element '"+setName+"' is missing unique name for memory.")
-            return;
-        }
-
-		if (elements[i].tagName == "INPUT" && elements[i].getAttribute("type") == "checkbox") {
-			setValue = elements[i].checked;
-		}
-		else {
-			setValue = elements[i].value;
-		}
-
-		if (action == "Save") {
-			localStorage.setItem(setName,setValue);
-		}
-		else if (action == "Restore") {
-            if (elements[i].tagName == "INPUT" && elements[i].getAttribute("type") == "checkbox") {
-                let item = JSON.parse(localStorage.getItem(setName));
-                if (item != null) {
-                    elements[i].checked = JSON.parse(localStorage.getItem(setName));
-                }
-            }
-            else {
-                let item = localStorage.getItem(setName);
-                if (item != null) {
-                    elements[i].value = item;
-                }
-            }
-		}
-	}
-
-}
-
-
-
-function memoryDexSwitch() {
-	let radio = document.getElementsByName("finaldex-dexswitch-" + GameID);
-	let val = localStorage.getItem("finaldex-dexswitch-" + GameID);
-
-
-	for(let i = 0; i < radio.length; i++) {
-		if(radio[i].value == val) {
-			radio[i].click();
-		} else if(val == null) {
-			radio[radio.length - 1].click();
-		}
-	}
-	$('input[name="finaldex-dexswitch-' + GameID + '"]').on("change", function() {
-		localStorage.setItem("finaldex-dexswitch-" + GameID, $(this).val());
-	});
-
-	if (val == null) {
-		radio[0].nextElementSibling.click();
-	}
+    memory_restore({ Elements: pokemon, Group: [config.Game, 'Dex'] }); // Restore state
 }
