d59e62098ab4906292f10659adcf590af9f63731
finaldex <123470609+finaldex@users.noreply.github.com>
2024-10-31 01:16:12 +0100

    Update item.js

--- js/item.js
+++ js/item.js
@@ -8,12 +8,12 @@ const create_item = function() {
 
     // Search
     const item_searchWrap = create_element({ Tag: "div", Class: ["search"], Parent: item_catalogHeader });
-    const item_searchInput = create_element({ Tag: "input", Attribute: { type: "search", name: "item_search", autocomplete: "off", placeholder: "Search Items...", onfocus: "this.placeholder=''", onblur: "this.placeholder='Search Items...'" }, Event: {input: event => search({ Entries: document.querySelectorAll('#item .catalog ol li'), Data: data.Items, }, event), keydown: event => search_filter({ Entries: document.querySelectorAll('#item .catalog ol li') }, event), }, Parent: item_searchWrap });
+    const item_searchInput = create_element({ Tag: "input", Attribute: { type: "search", name: "item_search", autocomplete: "off", placeholder: "Search Items...", onfocus: "this.placeholder=''", onblur: "this.placeholder='Search Items...'" }, Event: {input: event => search({ Entries: document.querySelectorAll('#item .catalog ol li'), Data: Data.Items, }, event), keydown: event => search_filter({ Entries: document.querySelectorAll('#item .catalog ol li') }, event), }, Parent: item_searchWrap });
 
     // Pocket
     const item_pocketWrap = create_element({ Tag: "div", Class: ["pocket"], Parent: item_catalogHeader });
 
-    const pockets = [...new Set(Object.values(data.Items).map(item => item.Pocket).filter(Boolean))];
+    const pockets = [...new Set(Object.values(Data.Items).map(item => item.Pocket).filter(Boolean))];
     pockets.forEach(pocket => {
         const item_pocketLabel = create_element({ Tag: "label", Attribute: { for: `item_pocket-${pocket}`, }, Parent: item_pocketWrap });
         const item_pocketInput = create_element({ Tag: "input", Attribute: { type: "checkbox", name: "item_pocket", id: `item_pocket-${pocket}`, value: pocket, checked: "" }, Event: {change: () => pocket_filter() }, Parent: item_pocketLabel });
@@ -25,18 +25,18 @@ const create_item = function() {
     const item_catalogList = create_element({ Tag: "ol",  Parent: item_catalog });
 
     // Item
-    const entries = Object.keys(data.Items);
+    const entries = Object.keys(Data.Items);
     entries.forEach((idx, i) => {
-        if (data.Items[idx] && data.Items[idx].Item) { 
-            const machineMove = Object.keys(data.Moves).find(key => data.Items[idx].Item.includes(data.Moves[key].Machine));
-            const item_src = get_directory({FirstMatch: true, Exact: true, File: [data.Items[idx].Image,...data.Items[idx].Item], Path: [path.Item.Bag]});
-            const machineType = machineMove ? data.Moves[get_moveIndex(machineMove)].Type : null;
+        if (Data.Items[idx] && Data.Items[idx].Item) { 
+            const machineMove = Object.keys(Data.Moves).find(key => Data.Items[idx].Item.includes(Data.Moves[key].Machine));
+            const item_src = get_directory({FirstMatch: true, Exact: true, File: [Data.Items[idx].Image,...Data.Items[idx].Item], Path: [path.Item.Bag]});
+            const machineType = machineMove ? Data.Moves[get_moveIndex(machineMove)].Type : null;
 
-            const item_catalogEntry = create_element({ Tag: "li",  Data: {index: idx, search: [...data.Items[idx].Item,...(machineMove != null ? [machineMove] : [])].join(",") }, Parent: item_catalogList });
+            const item_catalogEntry = create_element({ Tag: "li",  Data: {index: idx, search: [...Data.Items[idx].Item,...(machineMove != null ? [machineMove] : [])].join(",") }, Parent: item_catalogList });
             const item_catalogEntryLabel = create_element({ Tag: "label", Attribute: { for: `item_entry-${idx}`, }, Parent: item_catalogEntry });
             const item_catalogEntryInput = create_element({ Tag: "input", Attribute: { type: "radio", name: "item_entry", id: `item_entry-${idx}`, ...(i === 0 && {checked: ""}) }, Event: {change: () => item_data() }, Parent: item_catalogEntryLabel });
             const item_catalogEntryImage = create_element({ Tag: "img", Attribute: { src: item_src }, Parent: item_catalogEntryLabel });
-            const item_catalogEntryText = create_element({ Tag: "strong", HTML: machineMove ? `${data.Items[idx].Item}<span data-type='${machineType}'>${machineMove}</span>` : data.Items[idx].Item, Parent: item_catalogEntryLabel });
+            const item_catalogEntryText = create_element({ Tag: "strong", HTML: machineMove ? `${Data.Items[idx].Item}<span data-type='${machineType}'>${machineMove}</span>` : Data.Items[idx].Item, Parent: item_catalogEntryLabel });
         }
     });
 
@@ -73,7 +73,7 @@ function item_data() {
     if (!active_entry || !active_entry.dataset.index) { return }
     
     const index = active_entry.dataset.index
-    const item = data.Items[index].Item
+    const item = Data.Items[index].Item
 
     const titleText = document.querySelector("#item > header .title_text")
     titleText.innerText = item[0];
@@ -84,7 +84,7 @@ function item_data() {
     const priceElement = document.querySelector("#item > header .price")
     priceElement.innerHTML = "";
 
-    const item_price = data.Items[index].Sell ? data.Items[index].Sell : [];
+    const item_price = Data.Items[index].Sell ? Data.Items[index].Sell : [];
     item_price.forEach(d => {
         const currency_source = get_directory({FirstMatch: true, File: [d.Currency], Path: [path.Currency.Icon]});
         const currency = d.Currency == "N/A" ? "" : currency_source !== "" ? ` <img src='${currency_source}' title='${d.Currency}' />` : ` ${d.Currency}`;
@@ -95,31 +95,31 @@ function item_data() {
     const list = document.querySelector("#item .sidebar > main ul");
     list.innerHTML = ""; // Clear previous data
 
-    const item_location = Object.keys(data.Locations).reduce((found, index) => (data.Locations[index].Item && data.Locations[index].Item.some(i => item.some(it => i.Item && i.Item.includes(it))) ? (found[index] = data.Locations[index], found) : found), {});
+    const item_location = Object.keys(Data.Locations).reduce((found, index) => (Data.Locations[index].Item && Data.Locations[index].Item.some(i => item.some(it => i.Item && i.Item.includes(it))) ? (found[index] = Data.Locations[index], found) : found), {});
     generate_locationItem({Catalog:"Item", Entry: item, Data: item_location, Parent:list, });
 
-    const item_shop = Object.keys(data.Locations).reduce((found, index) => (data.Locations[index].Shop && data.Locations[index].Shop.some(i => item.some(it => i.Item && i.Item.includes(it))) ? (found[index] = data.Locations[index], found) : found), {});
+    const item_shop = Object.keys(Data.Locations).reduce((found, index) => (Data.Locations[index].Shop && Data.Locations[index].Shop.some(i => item.some(it => i.Item && i.Item.includes(it))) ? (found[index] = Data.Locations[index], found) : found), {});
     generate_locationShopItem({Catalog:"Item", Entry: item, Data: item_shop, Parent:list, });
 
-    const item_pickup = data.Abilities && data.Abilities["Pickup"] ? {"Pickup": data.Abilities["Pickup"]} : {};
+    const item_pickup = Data.Abilities && Data.Abilities["Pickup"] ? {"Pickup": Data.Abilities["Pickup"]} : {};
     generate_pickup({Catalog:"Item", Entry: item, Data: item_pickup, Parent: list, })
 
 
     const description = document.querySelector("#item > main");
     description.innerHTML = "";
 
-    const descriptionText = data.Items[index].Description ? create_element({ Tag: "p", Text: data.Items[index].Description, Class: ["description_text"], Parent: description }) : null;
+    const descriptionText = Data.Items[index].Description ? create_element({ Tag: "p", Text: Data.Items[index].Description, Class: ["description_text"], Parent: description }) : null;
 
-    const machineMove = Object.keys(data.Moves).find(key => item.includes(data.Moves[key].Machine));
+    const machineMove = Object.keys(Data.Moves).find(key => item.includes(Data.Moves[key].Machine));
     const machineText = machineMove ? create_element({ Tag: "p", HTML: `${item} contains the move <span>${machineMove}</span>.`, Parent: description }) : null;
 
     const machineElement = machineText ? machineText.querySelector(":scope span") : null
     machineElement && (add_redirect(machineElement,{catalog:"move",entry:machineMove,style:"invert"}));
 
-    const item_effect = data.Items[index].Effect ? data.Items[index].Effect : [];
+    const item_effect = Data.Items[index].Effect ? Data.Items[index].Effect : [];
     const effectTitle = item_effect.length > 0 ? create_element({ Tag: "h3", Text:"Effect", Class: ["effect_header"], Parent: description }) : null;
     item_effect.forEach(data => {
-        const effectText = create_element({ Tag: "p", Text: data.Effect, Class: ["effect_text"], Parent: description });
+        const effectText = create_element({ Tag: "p", Text: Data.Effect, Class: ["effect_text"], Parent: description });
     });
 
 
@@ -136,7 +136,7 @@ function pocket_filter() {
 
     entries.forEach(entry => {
         const index = entry.dataset.index;
-        const pocket = data.Items[index]?.Pocket;
+        const pocket = Data.Items[index]?.Pocket;
 
         if (activePockets.has(pocket)) {
             entry.classList.remove("filtered");
