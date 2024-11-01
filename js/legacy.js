30b3aa7947cff83638ae52a7a812a0ec4ed7ce16
finaldex <123470609+finaldex@users.noreply.github.com>
2024-11-01 22:30:44 +0100

    Update legacy.js

--- js/legacy.js
+++ js/legacy.js
@@ -261,4 +261,18 @@ Object.fromEntries(
         ])
         .filter(([key, values]) => values.length > 0)
 );
+
+Object.fromEntries(
+    Object.entries(finaldata.Directory)
+        .filter(([key]) => key.includes("/Location/"))
+        .map(([key, values]) => [
+            key,
+            values.filter(value => {
+                const match = value.match(/^(.*?)(?:_|(?=\.[^.]*$))/);
+                const baseValue = match ? match[1] : value;
+                return ![...new Set(finaldata.Locations.Overview.map(item => item.Location))].includes(baseValue);
+            })
+        ])
+        .filter(([key, values]) => values.length > 0)
+);
 */
